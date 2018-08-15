36d3c4bd40d701f5baa33620d4deb04a0ccfccfc
admin <admin@bloomnesia.com>
2018-08-15 20:53:43 +0800

    modified file `www.bloomnesia.com.php` (after deactivation of `Autoptimize` version 2.3.4)

--- wp-content/wp-rocket-config/www.bloomnesia.com.php
+++ wp-content/wp-rocket-config/www.bloomnesia.com.php
@@ -6,7 +6,7 @@ $rocket_cache_mobile = '1';
 $rocket_do_caching_mobile_files = '1';
 $rocket_cache_ssl = '1';
 $rocket_cache_reject_uri = '(.*)/feed/?|/checkout/(.*)|/cart/|/my-account/(.*)|/wc-api/v(.*)|/(index\.php/)?wp\-json(/.*|$)';
-$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_cookies = 'woocommerce_items_in_cart|wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
 $rocket_cache_reject_ua = 'facebookexternalhit';
 $rocket_cache_query_strings = array (
 );
